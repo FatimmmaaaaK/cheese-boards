a72c2a9111117cb541f8d8f8b131d71962d3110c
Kat Law <kathleen.law@multiverse.io>
2023-03-19 15:52:42 -0400

    adds code review comments

--- db-cheesey/db-cheesey.js
+++ db-cheesey/db-cheesey.js
@@ -3,6 +3,9 @@ const path = require('path')
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
+    // no need to include 'users/fatoumata.kaba' in the storage path,
+    // as other users running the app wouldn't be able to create or 
+    // navigate to this directory
     storage:'/Users/fatoumata.kaba/cheese-boards/db.sqlite'
 
 })
--- db-cheesey/models/index.js
+++ db-cheesey/models/index.js
@@ -3,20 +3,16 @@ const {Board} = require('./Board');
 const {Cheese} = require('./Cheese');
 const {sequelize} = require('../db-cheesey');
 
-
-
-
 User.hasMany(Board);
 Board.belongsTo(User);
 
-
+// this could be renamed to cheese_boards or a more descriptive 
+// join table name to link boards and cheeses?
+// this may help with the commented-out tests as well
 const CreativeDesign =sequelize.define('creativeDesign',{ });
 
 Board.belongsToMany(Cheese, {through:CreativeDesign});
 Cheese.belongsToMany(Board, {through:CreativeDesign});
-
-
-
 
 module.exports = {
     User,
